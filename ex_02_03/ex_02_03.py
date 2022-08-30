91416069ce611c3d3083f17e87f7021b6de64916
duke13 <togo13duke@gmail.com>
2022-08-30 13:55:01 +0900

    ex02_03

---
+++ ex_02_03/ex_02_03.py
@@ -0,0 +1,7 @@
+# This first line is provided for you
+input_hrs = input("Enter Hours:")
+hours = float(input_hrs)
+input_rt = input("Enter Rate:")
+rate = float(input_rt)
+pay = hours * rate
+print("Pay: ", pay)
