58789e76fa623eb85391d3cca1f81be9b9a4e3d7
duke13 <togo13duke@gmail.com>
2022-10-01 13:44:55 +0900

    ex15

---
+++ geodata/where.js
@@ -0,0 +1,12 @@
+myData = [
+[35.740747,139.7281611, 'Nishisugamo, Toshima City, Tokyo 170-0001, Japan'],
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[30.0185741,31.5013996, 'Plot 15 Admin building (South tower) 90 Axis, Beside FUE, in frond of AUC قسم أول القاهرة الجديدة، محافظة القاهرة‬ 4728120،، New Cairo 1, Cairo Governorate 4728120, Egypt'],
+[33.4242399,-111.9280527, 'Tempe, AZ 85281, USA'],
+[38.0399391,23.8030901, 'Monumental Plaza, Building C, 1st Floor, Leof. Kifisias 44, Marousi 151 25, Greece'],
+[28.3588163,75.58802039999999, 'Vidya Vihar, Pilani, Rajasthan 333031, India'],
+[6.8920758,3.7181452, '121103, Ilishan-Remo, Nigeria'],
+[25.2677203,82.99125819999999, 'Ajagara, Varanasi, Uttar Pradesh 221005, India'],
+[12.9504048,77.5020617, 'Mysore Rd, Jnana Bharathi, Bengaluru, Karnataka 560056, India']
+];
